f39549542f3a31fa4232b1b9452576eae1c45856
vwillyams <v.willyams@gmail.com>
2015-06-27 12:10:23 +1200

    added angular-ui-router since it will definitely be needed shortly

--- app/app.js
+++ app/app.js
@@ -1,7 +1,36 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.version'
-])
+var myApp = angular.module('myApp', [
+  'ui.router',
+]);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+      .state('state1', {
+        url: "/state1",
+        templateUrl: "partials/state1.html"
+      })
+      .state('state1.list', {
+        url: "/list",
+        templateUrl: "partials/state1.list.html",
+        controller: function($scope) {
+          $scope.items = ["A", "List", "Of", "Items"];
+        }
+      })
+      .state('state2', {
+        url: "/state2",
+        templateUrl: "partials/state2.html"
+      })
+      .state('state2.list', {
+        url: "/list",
+        templateUrl: "partials/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      });
+});
---
+++ app/components/routing/text-router.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Victor on 27/06/2015.
+ */
+'use strict';
+
+angular.module('text.router', []).controller('text-router', function(){
+
+});
